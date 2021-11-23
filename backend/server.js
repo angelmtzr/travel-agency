c082098b71d0b9158f25188cb0dd67b709a3f285
angelmtzr <angel.mtz.r02@gmail.com>
2021-11-23 17:14:23 -0600

    maybe deploy?

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,10 @@ app.use("/api/flights", flightRoutes);
 
 // --------------------------deployment------------------------------
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    console.log(__dirname);
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
     });
 } else {
     app.get("/", (req, res) => {
