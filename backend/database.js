7c91f7445df0463723402664f1a77a3c453085ea
angelmtzr <angel.mtz.r02@gmail.com>
2021-11-23 19:07:13 -0600

    better comments and erased trash modules

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
-const colors = require('colors');
-
-
-const URI = process.env.MONGODB_URI ||
-    "mongodb://localhost/databaseTest";
+require('colors');
 
 const connectDB = async () => {
     try {
+        const URI = process.env.MONGODB_URI || 
+            "mongodb://localhost/databaseTest";
         const conn = await mongoose.connect(URI, { useNewUrlParser: true });
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
     } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const dotenv = require("dotenv");
 const connectDB = require('./database');
-const colors = require('colors');
 const path = require("path");
+require('colors');
 
-// import routes
+// import routers
 const userRoutes = require("./routes/userRoutes");
 const flightRoutes = require("./routes/flightRoutes");
 
+// config to use environment variables --> .env or in heroku config vars
 dotenv.config();
 
+// connects to the database
 connectDB();
 
+// creates an express app
 const app = express();
 
+// allows us to recieve json data --> to parse info
 app.use(express.json());
 
+// use routers in our app
 app.use("/api/users", userRoutes);
 app.use("/api/flights", flightRoutes);
 
-// --------------------------deployment------------------------------
+// deployment configuration
 if (process.env.NODE_ENV === "production") {
-    console.log(__dirname);
     app.use(express.static(path.join(__dirname, "../frontend/build")));
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
@@ -31,14 +35,18 @@ if (process.env.NODE_ENV === "production") {
         res.send("API is running...");
     });
 }
-// --------------------------deployment------------------------------
 
+
+// gets the por to run our app
+// either with an environment variables or with a given port number
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-    PORT,
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
-            .bold
-    )
-);
+
+const main = async () => {
+    await app.listen(PORT); // make our app listen to an specific port
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow .bold);
+}
+
+main();
+
+
